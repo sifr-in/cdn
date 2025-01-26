e8cc17e995a2552eb65710ba6300171252f16efa
masoom sanadi owner at www.sifr.in <93422221+sifr-in@users.noreply.github.com>
2025-01-26 16:54:41 +0530

    Update e.js

--- ess/e.js
+++ ess/e.js
@@ -17,3 +17,28 @@ function loadScript(url, callback) {
   script.src = url;
   document.getElementsByTagName("head")[0].appendChild(script);
 }
+
+function postCall_Json(rURL, jsonData, tkrq, syncOrNo) {
+  var retTxt = "";
+  if (tkrq == 1) {
+    if (my1uzr.mk.length < 1)
+      showLogIn();
+    else {
+      const longNumber = parseInt(my1uzr.mk.slice(-10), 10);
+      checkExpiry(longNumber);
+    }
+  }
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", rURL, syncOrNo);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      retTxt = xhr.responseText;
+      loader.style.display = 'none';
+    }
+  };
+  loader.style.display = 'flex';
+  xhr.send(JSON.stringify(jsonData));
+  return retTxt;
+}
