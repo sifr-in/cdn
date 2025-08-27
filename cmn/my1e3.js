ec18ad05899274bab25873e5d4acd594e1055c5b
masoom sanadi owner at www.sifr.in <93422221+sifr-in@users.noreply.github.com>
2025-08-27 15:40:50 +0530

    Update my1e3.js

--- cmn/my1e3.js
+++ cmn/my1e3.js
@@ -850,7 +850,7 @@ addLongPressListener(modalTitle, function() {
 
 function chkMyControlExists() {
  if (typeof showMyControls !== 'function') {
-  loadScript("https://cdn.jsdelivr.net/gh/sifr-in/cdn@5f6e9b7/cmn/my1ctr.js", () => {
+  loadScript("https://cdn.jsdelivr.net/gh/sifr-in/cdn@d875c15/cmn/my1ctr.js", () => {
    if (typeof showMyControls === 'function') {
     showMyControls();
    } else {
@@ -983,6 +983,7 @@ function addLongPressListener(element, callback, duration = 500) {
     // Prevent context menu
     element.addEventListener('contextmenu', (e) => e.preventDefault());
 }
+
 
 
 
