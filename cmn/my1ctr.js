fa1480fa9fa533af3f0b84203de6c03143a0767a
masoom sanadi owner at www.sifr.in <93422221+sifr-in@users.noreply.github.com>
2025-08-27 16:17:09 +0530

    my1ctr

--- cmn/my1ctr.js
+++ cmn/my1ctr.js
@@ -55,7 +55,8 @@ buttonsContainer.style.gap = '10px';
 
 // Create buttons for items where vn.length > 0
 my1uzr.fnf.forEach(item => {
-if (my1uzr.mo.toString() === item.e.toString() && my1uzr.mc.toString() === item.f.toString() && item.vn && item.vn.length > 0) {
+const idVw = id_of_view || 1;
+if (my1uzr.mo.toString() === item.e.toString() && my1uzr.mc.toString() === item.f.toString() && item.va === idVw && item.vn && item.vn.length > 0) {
   const button = document.createElement('button');
   button.textContent = item.vn;
   button.style.padding = '8px 12px';
