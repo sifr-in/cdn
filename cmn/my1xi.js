19fd73deaeae2b1facb28c0aa3d479741d05f5cc
masoom sanadi owner at www.sifr.in <93422221+sifr-in@users.noreply.github.com>
2026-04-16 21:36:48 +0530

    Update my1xi.js

--- cmn/my1xi.js
+++ cmn/my1xi.js
@@ -284,7 +284,7 @@ class DexieDBManager {
 
   // 2. Fetch from remote if not found locally
   try {
-   const response = await fetch("https://cdn.jsdelivr.net/gh/sifr-in/cdn@9be88db/cmn/my1xi.da");
+   const response = await fetch("https://cdn.jsdelivr.net/gh/sifr-in/cdn@de32eef/cmn/my1xi.da");
    if (response.ok) {
     const remoteSchemas = await response.json();
     schema = Array.isArray(remoteSchemas)
